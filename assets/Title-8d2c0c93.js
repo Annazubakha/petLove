ac833f6913c3f02ea95bc01c3094a57dc7710e7d
Annazubakha <145809450+Annazubakha@users.noreply.github.com>
2024-07-18 14:49:58 +0000

    Deploying to gh-pages from @ Annazubakha/petLove@07b0c6451ae15ac0d94912189566d723ff8b105c 🚀

--- assets/Title-8d2c0c93.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-2afbff62.js";const x=({children:t})=>e.jsx("h1",{className:"font-bold text-[28px] md:text-[54px] tracking-[-0.03em] ",children:t});export{x as T};
---
+++ assets/Title-e0181d68.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-18903a09.js";const x=({children:t})=>e.jsx("h1",{className:"font-bold text-[28px] md:text-[54px] tracking-[-0.03em] ",children:t});export{x as T};
